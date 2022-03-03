599df8b6a60c5601fcc0ec06a28bc4365d917a5c
Mike Dalton <michaelcdalton@gmail.com>
2022-03-02 22:21:23 -0500

    Switching to d3-dag which has more natural layout for DAGs
    Based on https://codesandbox.io/s/4hixc

--- src/App.js
+++ src/App.js
@@ -1,89 +1,171 @@
 import React from 'react';
-import * as d3 from "d3";
-import './App.css';
+import * as d3Base from "d3";
+import * as d3Dag from "d3-dag";
+import { Group } from "@visx/group";
+import { LinePath, Circle } from "@visx/shape";
+import { Text } from "@visx/text";
+import { curveCatmullRom } from "@visx/curve";
+import { MarkerArrow } from "@visx/marker";
+
+// serialize to parent ids
+// collapse node - remove node, add connections between all adjacent nodes
+// collapsing may lead to cycles - how to handle it?
+
+const data = [
+  {
+    id: "0",
+    parentIds: ["8"],
+    color: "blue"
+  },
+  {
+    id: "1",
+    parentIds: []
+  },
+  {
+    id: "2",
+    parentIds: []
+  },
+  {
+    id: "3",
+    parentIds: ["11"]
+  },
+  {
+    id: "4",
+    parentIds: ["12"]
+  },
+  {
+    id: "5",
+    parentIds: ["18"]
+  },
+  {
+    id: "6",
+    parentIds: ["9", "15", "17"]
+  },
+  {
+    id: "7",
+    parentIds: ["3", "17", "20", "21"]
+  },
+  {
+    id: "8",
+    parentIds: []
+  },
+  {
+    id: "9",
+    parentIds: ["4"],
+    color: "green"
+  },
+  {
+    id: "10",
+    parentIds: ["16", "21"]
+  },
+  {
+    id: "11",
+    parentIds: ["2"]
+  },
+  {
+    id: "12",
+    parentIds: ["21"]
+  },
+  {
+    id: "13",
+    parentIds: ["4", "12"]
+  },
+  {
+    id: "14",
+    parentIds: ["1", "8"]
+  },
+  {
+    id: "15",
+    parentIds: []
+  },
+  {
+    id: "16",
+    parentIds: ["0"]
+  },
+  {
+    id: "17",
+    parentIds: ["19"]
+  },
+  {
+    id: "18",
+    parentIds: ["9"]
+  },
+  {
+    id: "19",
+    parentIds: []
+  },
+  {
+    id: "20",
+    parentIds: ["13"]
+  },
+  {
+    id: "21",
+    parentIds: []
+  }
+];
+
+const d3 = Object.assign({}, d3Base, d3Dag);
+const width = 800;
+const height = 800;
+
+const d = d3Dag.dagStratify()(data);
+const layout = d3
+  .sugiyama()
+  .size([width, height])
+  .decross(d3.decrossTwoLayer())
+  .coord(d3.coordGreedy())
+  // .layering(d3.layeringLongestPath())
+  // .decross(d3.decrossOpt())
+  // .coord(d3.coordVert())
+  .nodeSize(() => [20, 20]);
+
+const dag = layout(d).dag;
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.myReference = React.createRef();
-  }
-
-  componentDidMount() {
-    const filename = "equation_x1x2_sin_x2_graph.json"
-
-    function transform(d) {
-      return "translate(" + d.x + "," + d.y + ")";
-    }
-
-    const margin = { top: 100, right: 100, bottom: 100, left: 100 }
-    const width = 800 - margin.left - margin.right;
-    const height = 800 - margin.top - margin.bottom;
-
-    const svg = d3.select(this.myReference.current)
-      .append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform",
-        `translate(${margin.left}, ${margin.top})`);
-
-    d3.json(filename).then( function( data) {
-
-      // Initialize the links
-      const link = svg
-        .selectAll("line")
-        .data(data.links)
-        .join("line")
-        .style("stroke", "#aaa")
-
-      // Initialize the nodes
-      const node = svg
-        .selectAll("circle")
-        .data(data.nodes)
-        .join("circle")
-        .attr("r", 20)
-        .style("fill", "#69b3a2")
-
-      const text = svg
-        .selectAll("text")
-        .data(data.nodes)
-        .enter()
-        .append("text")
-        .text(function(d) { return d.name; });
-
-      // Let's list the force we wanna apply on the network
-      const simulation = d3.forceSimulation(data.nodes)                 // Force algorithm is applied to data.nodes
-        .force("link", d3.forceLink()                               // This force provides links between nodes
-          .id(function(d) { return d.id; })                     // This provide  the id of a node
-          .links(data.links)                                    // and this the list of links
-        )
-        .force("charge", d3.forceManyBody().strength(-400))         // This adds repulsion between nodes. Play with the -400 for the repulsion strength
-        .force("center", d3.forceCenter(width / 2, height / 2))     // This force attracts nodes to the center of the svg area
-        .on("end", ticked);
-
-      // This function is run at each iteration of the force algorithm, updating the nodes position.
-      function ticked() {
-        link
-          .attr("x1", function(d) { return d.source.x; })
-          .attr("y1", function(d) { return d.source.y; })
-          .attr("x2", function(d) { return d.target.x; })
-          .attr("y2", function(d) { return d.target.y; });
-
-        node.attr("transform", transform);
-        text.attr("transform", transform);
-      }
-
-    });
-  }
-
   render() {
     return (
-      <div>
-        <div ref={this.myReference} />
-      </div>
+      <svg width={width} height={height}>
+        <rect width={width} height={height} stroke="red" fill="#FFF" />
+        <MarkerArrow id="marker-arrow" fill="#333" refX={2} size={6} />
+        <Group top={0} left={0}>
+          <>
+            {dag.links().map((link) => (
+              <LinePath
+                key={`${link.source.id}-${link.target.id}`}
+                curve={curveCatmullRom}
+                data={link.points}
+                x={(d) => d.x}
+                y={(d) => d.y}
+                stroke="#333"
+                strokeWidth={2}
+                markerMid="url(#marker-arrow)"
+              />
+            ))}
+          </>
+          <>
+            {dag.descendants().map((d) => {
+              console.log("d", d);
+              return (
+                <Group key={d.id}>
+                  <Circle
+                    key={d.id}
+                    cx={d.x}
+                    cy={d.y}
+                    r={20}
+                    fill={d.data.color || "red"}
+                  />
+                  <Text
+                    x={d.x}
+                    y={d.y}>
+                    {d.id}
+                  </Text>
+                </Group>
+              );
+            })}
+          </>
+        </Group>
+      </svg>
     );
   }
 }
-
 export default App;
